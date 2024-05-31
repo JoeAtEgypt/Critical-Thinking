034e9179346cb2e8e12c825811c812b44ce152c3
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-05-31 17:37:40 +0300

    debugging merge sort

--- Sorting/selection.py
+++ Sorting/selection.py
@@ -1,6 +1,3 @@
-arr = [37, 45, 39, 49, 43, 5, 20, 19, 38, 30]
-
-
 def selection_sort(arr, size):
     for i in range(size - 1):
         min_index = i
@@ -8,7 +5,3 @@ def selection_sort(arr, size):
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[min_index], arr[i] = arr[i], arr[min_index]
-
-
-selection_sort(arr, 10)
-print(arr)
