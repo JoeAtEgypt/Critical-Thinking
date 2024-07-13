27902117b559968f9d678c940d18acfe2d50cd80
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-07-13 14:25:26 +0300

    implemented frequency array

---
+++ fequency_array.py
@@ -0,0 +1,17 @@
+arr = [1, 3, 2, 4, 2, 1]
+
+
+def get_frequency(arr):
+    freq = {}
+
+    for i in arr:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+
+    return freq
+
+
+if __name__ == "__main__":
+    print(get_frequency(arr))
