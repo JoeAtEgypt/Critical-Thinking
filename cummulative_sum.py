fe13d1d1c759d85eb496c99876d10654deec54c6
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-07-14 10:55:52 +0300

    implemented cummulative sum

--- cummulative_sum.py
+++ cummulative_sum.py
@@ -1,14 +1,29 @@
-arr = [1, 3, 9, 5, 2, 7, 4]
+arr = [1, 3, 9, 5, 7, 2, 4, 1]
 
 
 def prefix_sum(arr):
     size = len(arr)
-    prefix_sum = [arr[0]]
+    pre_sum = [arr[0]]
 
     for i in range(1, size):
-        prefix_sum.append(arr[i] + prefix_sum[i-1])
+        pre_sum.append(arr[i] + pre_sum[i-1])
 
-    return prefix_sum
+    return pre_sum
+
+def sum(pre, left, right):
+    if left == 0:
+        return pre[right]
+    if left <= right:
+        return pre[right] - pre[left-1]
+    else:
+        print("the left index must be smaller than or equal right index")
+        return -1
+
 
 if __name__ == "__main__":
-    print(prefix_sum(arr))
+    pre_sum = prefix_sum(arr)
+    print(pre_sum)
+    print(sum(pre_sum, 1, 4))
+    print(sum(pre_sum, 0, 7))
+    print(sum(pre_sum, 4, 4))
+    print(sum(pre_sum, 7, 4))
